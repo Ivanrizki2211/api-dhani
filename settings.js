bfc4ccee7aa1a733462b88e548bfdba7a99d34b0
Vanz Z <160609342+Ivanrizki2211@users.noreply.github.com>
2025-03-06 01:11:11 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ global.creator = 'Vanz'
 global.listkey = ["Vanz","turusek"]
 
 //isi agar fitur fitur bisa di gunakan.
-global.lolkey = 'ISI_PUNYA_LOLHUMAN' // Dapatkan di https://api.lolhuman.xyz
+global.lolkey = 'cbeee8ab442cd878f556948c' // Dapatkan di https://api.lolhuman.xyz
 global.botkey = 'Admin' // Dapatkan di https://api.botcahx.biz.id
 
 global.loghandler = {
